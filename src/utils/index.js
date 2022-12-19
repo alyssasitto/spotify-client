6c7c0cb450d67b30556245de868155b4ab95df60
Alyssa Sitto <alyssasitto1@gmail.com>
2022-12-19 01:26:55 -0800

    Change api url in utils folder

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5005";
+const API_URL = process.env.REACT_APP_API_URL;
 
 const token = localStorage.getItem("spotify_access_token");
 
